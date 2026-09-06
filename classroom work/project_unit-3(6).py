9906eb3580b0ec9e364272539cbe93606efa7928
Dhruv Prasad <dhruvpar34@gmail.com>
2026-09-06 18:31:59 +0530

    Add files via upload

---
+++ classroom work/project_unit-3(6).py
@@ -0,0 +1,58 @@
+# 1. Write a function greet() that prints "Welcome to Python Programming".
+
+# def greet():
+#     print("Welcome to python Programming")
+# greet()
+# print(greet())
+
+# 2. Write a function square(n) that accepts a number and returns its square.
+
+# def square(n):
+#     return n*n
+
+# print(square(3))
+
+# 3. Write a function is_even(n) that returns True if the number is even, otherwise False.
+
+# def is_even(n):
+#     if n%2 == 0 :
+#         return True
+#     else:
+#         return False
+# print (is_even(3))
+
+# 4. Write a function maximum(a, b) that returns the larger of two numbers.
+
+# def maximum (a,b):
+#     if a>b :
+#         return a 
+#     else:
+#         return b 
+# print(maximum(2,4))
+
+# 5. Write a function calculate_area(radius) that calculates and returns the area of a circle.
+
+# def calculate_area(radius):
+#     a = 3.14 *radius *radius
+#     return a 
+# print (calculate_area(3))
+
+# 6. Write a function calculate(a, b) that returns the sum, difference, product, and
+# division of two numbers.
+
+# def calculate(a,b):
+#     sum = a + b 
+#     if a>b :
+#         difference = a-b
+#     else :
+#         difference = b-a
+#     product = a*b
+#     division = a/b
+#     return sum,difference,product,division
+# print(calculate(3,4))
+
+# 7. Write a function student_result(marks) that accepts a list of marks and returns:
+# Total marks, Average marks, Highest marks, Lowest marks
+
+def student_result(marks):
+    
